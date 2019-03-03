67467d11e4ec6639092ff3081e5ca81449cfac69
Darek-Mac <darek.baranczuk@gmail.com>
2019-03-03 21:53:00 +0100

    Core clean up. Added basic analyze

--- pi-wifi-hostspots.py
+++ pi-wifi-hostspots.py
@@ -8,24 +8,30 @@ class WifiHotspotsDetect:
 	interval = 10
 	devices = { "laptop": "wlp4s0", "pi": "wlan0" }
 	scanCommand = "sudo iw dev {0} scan"
-	fileFormat = "{0}.log"
+	fileExtension = ".log"
+	fileFormat = "{0}{1}"
 
 	def start(self):
 		device = self.devices['pi'] if self.isPi else self.devices['laptop']
 		command = self.scanCommand.format(device)
 		while True:
-			result = None
-			if self.isDebug:
-				result = command
-			else:  
-				result = os.popen(command).read()
+			result = command if self.isDebug else os.popen(command).read()
 			self.saveResult(result)
 			time.sleep(self.interval)
 
 	def saveResult(self, result):
 		timestamp = int(time.time())
-		fileName = self.fileFormat.format(timestamp)
+		fileName = self.fileFormat.format(timestamp, self.fileExtension)
 		with open(fileName, 'w') as file:
 			file.write(result)
+			
+	def analyze(self):
+		directory = "./"
+		for filename in os.listdir(directory):
+			if filename.endswith(self.fileExtension):
+				with open(filename) as file:
+					content = file.readlines()
+					print(len(content))
 
 WifiHotspotsDetect().start()
+#WifiHotspotsDetect().analyze()
